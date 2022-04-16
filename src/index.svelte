e9282e7d8d43876e13056bde7384cb8c7d58ac72
aidant <15520814+aidant@users.noreply.github.com>
2022-04-16 17:06:01 +1000

    Initial commit

--- src/index.svelte
+++ src/index.svelte
@@ -1,1 +1,36 @@
-<h1>Hello World!</h1>
+<script lang="ts">
+  import { writable } from 'svelte/store'
+
+  const webhookURL = writable('')
+  const message = writable('')
+
+  const handleSubmit = () => {
+    fetch($webhookURL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+      body: JSON.stringify({
+        content: $message,
+      }),
+    })
+  }
+</script>
+
+<form class="flex flex-col gap-2 container" on:submit|preventDefault={handleSubmit}>
+  <label>
+    Webhook URL:
+    <br />
+    <input
+      class="border rounded border-black px-4 py-1 w-full"
+      type="text"
+      bind:value={$webhookURL}
+    />
+  </label>
+  <label>
+    Message:
+    <br />
+    <input class="border rounded border-black px-4 py-1 w-full" type="text" bind:value={$message} />
+  </label>
+  <button class="border rounded border-black px-4 py-1" type="submit">Send Message</button>
+</form>
